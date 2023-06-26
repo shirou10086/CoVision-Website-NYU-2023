abdc65b79ef00481f1d3ea4d79e3d4a2596c8a18
Jinli Duan <jinlid@uci.edu>
2023-06-26 01:07:56 -0400

    update

--- static/humanmap.js
+++ static/humanmap.js
@@ -1,1 +1,1 @@
-var humanmap = [["Adrian", 0, 5, 1, 1, 0], ["Adrian", 0, 2, 1, 1, 0], ["Adrian", 0, 4, 0, 1, 0], ["Andover", 0, 3, 2, 0, 1]];
+var humanmap = [["Adrian", 0, 5, 1, 1, 0], ["Adrian", 0, 2, 1, 1, 0], ["Adrian", 0, 4, 0, 1, 0], ["Andover", 0, 3, 2, 0, 1], ["Angiola", 0, 2, 0, 1, 0], ["Angiola", 0, 3, 0, 0, 1]];
